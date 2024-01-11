0aaf002292fb8b260bf501ecc00f6450b20aeae1
James Salvatore <jimmy.c.salvatore@gmail.com>
2024-01-11 15:54:28 -0600

    chore: updates interval defs;

--- interval-training/intervalTraining.js
+++ interval-training/intervalTraining.js
@@ -1,107 +1,87 @@
-function mod(n, m) {
-    // https://stackoverflow.com/a/17323608
-    return ((n % m) + m) % m;
-}
-
-const NOTES = [
-    "3-a", "3_a", "3_f", "4-f", "4_d", "5-c", "5_b",
-    "5_g", "3-c", "3_b", "3_g", "4-g", "4_e", "5-d",
-    "5_c", "6_c", "3-d", "3_c", "4-a", "4_a", "4_f",
-    "5-f", "5_d", "3-f", "3_d", "4-c", "4_b", "4_g",
-    "5-g", "5_e", "3-g", "3_e", "4-d", "4_c", "5-a",
-    "5_a", "5_f"
-]
-const NOTE_VALUES = ["a", "a#", "b", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#"]
-
-const INTERVAL_NAMES = {
-    "P1": "Perfect Unison",
-    "P4": "Perfect 4th",
-    "P5": "Perfect 5th",
-    "P8": "Perfect Octave",
-    "M2": "Major 2nd",
-    "M3": "Major 3rd",
-    "M6": "Major 6th",
-    "M7": "Major 7th",
-    "m2": "Minor 2nd",
-    "m3": "Minor 3rd",
-    "m6": "Minor 6th",
-    "m7": "Minor 7th",
-    "A1": "Minor 2nd", // Equivalent.
-    "A4": "Augmented 4th",
-    "d1": "Minor 2nd", // ???
-    "d5": "Diminished 5th"
-}
-
-// Used to see if we already got notes, before
-// the user resets them.
+// Used to see if we already got notes.
+// Of the form [startNote, endNote, interval].
 document.NOTEPACKS = null;
+
+// Synth for tone.js.
+document.SYNTH = null;
+
+function synthInit() {
+    if (document.SYNTH === null) {
+        document.SYNTH = new Tone.Synth().toDestination();
+    }
+}
 
 function noteReset() {
     console.log("Resetting...")
     document.NOTEPACKS = null;
 }
 
-function getRandomNotes() {
-    let randOctave = Math.floor(Math.random() * 2) + 3;
-    let randInterval = Math.floor(Math.random() * 24) - 12;
-
-    let randNoteIdx = Math.floor(Math.random() * NOTE_VALUES.length);
-    let randStartingNote = NOTE_VALUES[randNoteIdx];
-
-    let randEndingNoteIdx = mod((randNoteIdx + randInterval), 12);
-    let randEndingNote = NOTE_VALUES[randEndingNoteIdx];
-    var endingOctave = randOctave;  // Set default.
-
-    if ((randNoteIdx + randInterval) > 12) {
-        endingOctave = randOctave + 1
-    }
-    else if ((randNoteIdx + randInterval) < 0) {
-        endingOctave = randOctave - 1
-    }
-
-    let startingNote = randStartingNote + randOctave;
-    let endingNote = randEndingNote + endingOctave;
-
-    console.log(startingNote, endingNote)
-    return [startingNote, endingNote]
-}
-
-function play() {
-    const TIME_BETWEEN_NOTES = 500
-    if (document.NOTEPACKS === null) {
-        document.NOTEPACKS = getRandomNotes()
-    }
-
-    // Leave time between notes.
-    setTimeout(() => { playNotes(document.NOTEPACKS[0]) }, TIME_BETWEEN_NOTES);
-    playNotes(document.NOTEPACKS[1]);
-}
-
-function playNotes(note) {
-    let noteFile = note.replace("#", "-")
-    let audio = new Howl({
-        src: [`assets/audio/${noteFile}.mp3`],
-        autoplay: false,
-        loop: false,
-        volume: document.querySelector("#volume-control").value / 100
-    });
-    audio.play();
-}
-
-function getInterval() {
-    // This shouldn't happen, but...
+function generateNotePacksIfNull() {
     if (document.NOTEPACKS === null) {
         console.warn("Notepack not generated.  Generating one now...")
         document.NOTEPACKS = getRandomNotes()
+
+        // Assign the interval to the relevant p-tag.
+        let intervalValueItem = document.getElementById("interval-val");
+        intervalValueItem.innerText = `${document.NOTEPACKS[2]}`
+    }
+}
+
+function getCurrentNotes() {
+    generateNotePacksIfNull()
+    return [document.NOTEPACKS[0], document.NOTEPACKS[1]]
+}
+
+function getRandomNotes() {
+    // TODO: We have to have a lower bound here.
+    let randInterval = Math.floor(Math.random() * 24) - 12;
+
+    let randNoteIdx = Math.floor(Math.random() * NOTES.length);
+    let randStartingNote = NOTES[randNoteIdx];
+    let startingOctave = Math.floor(Math.random() * 2) + 3;
+
+    let startingNoteStr = randStartingNote + startingOctave.toString();
+
+    let startingNoteOctaveIndex = NOTES_WITH_OCTAVE.indexOf(startingNoteStr)
+    let endingNoteStr = NOTES_WITH_OCTAVE[startingNoteOctaveIndex + randInterval]
+
+    let positiveRandInterval = Math.abs(randInterval)
+    let intervalName = SEMITONE_TO_INTERVAL_MAPPING[positiveRandInterval]
+
+    console.log("Notes:", startingNoteStr, endingNoteStr, "Interval:", randInterval)
+
+    if ((startingNoteStr === undefined) || (endingNoteStr === undefined)) {
+        console.log("oh no.")
+        getRandomNotes()
+    }
+    else {
+
+        return [startingNoteStr, endingNoteStr, intervalName]
+    }
+}
+
+function playNote(note) {
+    synthInit();
+    document.SYNTH.triggerAttackRelease(note.toUpperCase(), "8n");
+}
+
+function playNotes(ascendingOnly) {
+    var notes = getCurrentNotes()
+    var note0 = notes[0];
+    var note1 = notes[1];
+
+
+    if (ascendingOnly) {
+        idx0 = NOTES_WITH_OCTAVE.indexOf(notes[0])
+        idx1 = NOTES_WITH_OCTAVE.indexOf(notes[1])
+
+        if (idx1 > idx0) {
+            note0 = notes[1]
+            note1 = notes[0]
+        }
     }
 
-    let interval = teoria.Interval.between(
-        teoria.note(document.NOTEPACKS[0]),
-        teoria.note(document.NOTEPACKS[1])
-    )
-    interval.toString();
-
-    console.log(interval.simple(true).toString());
-    let intervalValueItem = document.getElementById("interval-val");
-    intervalValueItem.innerText = `${INTERVAL_NAMES[interval.simple(true).toString()]}`
+    // Leave time between notes.
+    setTimeout(() => { playNote(note0) }, TIME_BETWEEN_NOTES);
+    playNote(note1);
 }
